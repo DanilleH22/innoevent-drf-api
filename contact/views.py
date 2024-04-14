fd98847d4a09411d360e76e4c116d9d359e94eea
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-14 08:51:59 +0000

    Refactor views to remove unused code

--- contact/views.py
+++ contact/views.py
@@ -8,15 +8,6 @@ from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status, permissions
 
-# Create your views here.
-
-# class ContactUs(APIView):
-#     def post(self, request):
-#         serializer = ContactSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ContactUs(CreateAPIView):
     queryset = Contact.objects.all()
