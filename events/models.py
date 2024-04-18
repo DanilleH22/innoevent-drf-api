a9dc7a71691e02d143a301d282817c576928a5c8
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-18 17:10:20 +0000

    Change owner to USer

--- events/models.py
+++ events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from profiles.models import Profile
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,11 +11,4 @@ class Events(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_profile_qdjgyp'
         )
-    owner = models.ForeignKey(Profile, related_name='owned_events', on_delete=models.CASCADE)
-
-
-class SignUp(models.Model):
-    event = models.ForeignKey(Events, on_delete=models.CASCADE)
-    attendee = models.ForeignKey(Profile, related_name='signed_up_events', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
+    owner = models.ForeignKey(User, related_name='owned_events', on_delete=models.CASCADE)
