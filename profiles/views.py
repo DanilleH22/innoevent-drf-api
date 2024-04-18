04ba07ea73e2553bb16a31d3c50695419820d904
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-18 17:00:01 +0000

    Fix def get in views

--- profiles/views.py
+++ profiles/views.py
@@ -1,19 +1,10 @@
-from django.http import Http404
-from rest_framework import status, generics
-from rest_framework.views import APIView
+# from innoevent.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
-from innoevent.permissions import IsOwnerOrReadOnly
-from events.models import Events
-from signup.models import SignUp
-from events.serializers import EventSerializer
-from signup.serializers import SignUpSerializer
-from rest_framework.permissions import IsAuthenticated 
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_list_or_404, get_object_or_404
-from django.contrib.auth.models import User
-
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
@@ -23,30 +14,12 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
         pk = self.kwargs.get('pk')
         return get_object_or_404(Profile, pk=pk)
 
-    def get(self, request, pk):
+    def get(self, request, *args, **kwargs):
         profile = self.get_object()
         self.check_object_permissions(request, profile)
-        profile_serializer = ProfileSerializer(profile, context={'request': request})
+        serializer = ProfileSerializer(profile, context={'request': request})
+        return Response(serializer.data)
 
-        if request.user.profile.id == int(pk):
-            # Events created by the user
-            events_created = Events.objects.filter(owner=profile.pk)
-            events_created_serializer = EventSerializer(events_created, many=True, context={'request': request})
-
-            # Events the user has signed up for
-            signups = SignUp.objects.filter(attendee=profile.pk)
-            event_ids = signups.values_list('event', flat=True)
-            events_signed_up = Events.objects.filter(id__in=event_ids)
-            events_signed_up_serializer = EventSerializer(events_signed_up, many=True, context={'request': request})
-
-            return Response({
-                'Profile': profile_serializer.data,
-                'Events_created': events_created_serializer.data,
-                'Signed_up_for': events_signed_up_serializer.data
-            })
-
-        return Response({'Profile': profile_serializer.data})
-    
     def put(self, request, *args, **kwargs):
         profile = self.get_object()
         serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
