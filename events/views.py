c9f543bcd65db5c49294ebf3691ba92d78f47b12
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-20 20:01:48 +0000

    Fix permission classes for events

--- events/views.py
+++ events/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from .models import Events
 from .serializers import EventSerializer
-from rest_framework.permissions import IsAuthenticated 
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from innoevent.permissions import IsOwnerOrReadOnly
 from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework.permissions import IsAdminUser
 from rest_framework import generics, permissions
@@ -12,19 +13,16 @@ from django.contrib.auth.models import User
 
 class EventList(generics.ListCreateAPIView):
     serializer_class = EventSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Events.objects.all().order_by('date')
 
     def perform_create(self, serializer):
-        user = self.request.user  # Get the user instance
-        serializer.save(owner=user)
-
-
+        serializer.save(owner=self.request.user)
 
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Events.objects.all().order_by('date')
     serializer_class = EventSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self):
         obj = super().get_object()
