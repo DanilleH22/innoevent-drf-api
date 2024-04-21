d641637710ea41c76e9d2ecb9311c7cbec53e286
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-21 16:16:31 +0000

    Add trailing slash

--- events/urls.py
+++ events/urls.py
@@ -3,6 +3,6 @@ from events import views
 
 urlpatterns = [
     path('events/', views.EventList.as_view()),
-    path('events/create', views.EventCreate.as_view()),
+    path('events/create/', views.EventCreate.as_view()),
     path('events/<int:pk>/', views.EventDetail.as_view()),
 ]
