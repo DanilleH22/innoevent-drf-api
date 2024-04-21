65d4b3d281fc6cb19a28b9785af63f91a6ce51c9
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-21 15:59:32 +0000

    Add events create page

--- events/views.py
+++ events/views.py
@@ -3,12 +3,13 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from .models import Events
 from .serializers import EventSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from innoevent.permissions import IsOwnerOrReadOnly
 from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework.permissions import IsAdminUser
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
+from rest_framework.parsers import MultiPartParser, FormParser
 
 
 class EventList(generics.ListCreateAPIView):
@@ -16,6 +17,12 @@ class EventList(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Events.objects.all().order_by('date')
 
+
+class EventCreate(generics.CreateAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+    
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
