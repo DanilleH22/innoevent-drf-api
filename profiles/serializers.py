4adf4f17cc0cb399bd75d608c7b82373da9e3469
Danille Hamilton <danilleham@hotmail.co.uk>
2024-05-30 20:34:34 +0000

    Add create events and signup to serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,6 +6,8 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    created_events = EventSerializer(many=True, read_only=True, source='user.created_events')
+    signed_up_events = SignUpSerializer(many=True, read_only=True, source='user.signups')
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -14,5 +16,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'biography', 'is_owner'
+            'id', 'owner', 'biography', 'is_owner', 'created_events',
+            'signed_up_events'
         ]
