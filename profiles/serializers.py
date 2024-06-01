14c85f184c6e719ea8caa5ce4aeac7599c9cabf3
Danille Hamilton <danilleham@hotmail.co.uk>
2024-06-01 10:29:49 +0000

    Refactor profileserializer meta

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,12 +2,9 @@ from rest_framework import serializers
 from .models import Profile
 
 
-
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    created_events = EventSerializer(many=True, read_only=True, source='user.created_events')
-    signed_up_events = SignUpSerializer(many=True, read_only=True, source='user.signups')
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -16,6 +13,5 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'biography', 'is_owner', 'created_events',
-            'signed_up_events'
+            'id', 'owner', 'biography', 'is_owner',
         ]
