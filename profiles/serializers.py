e2f0457fef8b965aafe56e41ce0d68b23bb0f080
Danille Hamilton <danilleham@hotmail.co.uk>
2024-06-01 15:51:41 +0000

    Add a def get_signed_up

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
 from .models import Profile
+from signup.models import SignUp
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    signed_up = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_signed_up(self, obj):
+        from events.serializers import EventSerializer
+        signups = SignUp.objects.filter(attendee=obj.owner)
+        events = [signup.event for signup in signups]
+        return EventSerializer(events, many=True, context=self.context).data
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'biography', 'is_owner',
+            'signed_up'
         ]
